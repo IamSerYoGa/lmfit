IamSerYoGa/lmfit

commit 16341898ffcc4006ed79081819d01b997ad8f9d8

definitely a simplification: eliminate SQR(sqrt)

lib/lmmin.c
@@ -394,12 +394,12 @@ void lmmin( int n, double *x, int m, const void *data,
                 for (i = 0; i <= j; i++)
                     wa3[i] -= fjac[j*m+i] * wa1[ipvt[j]];
             }
-            temp1 = lm_enorm(n, wa3) / fnorm;
-            temp2 = sqrt(par) * pnorm / fnorm;
-            prered = SQR(temp1) + 2 * SQR(temp2);
+            temp1 = SQR( lm_enorm(n, wa3) / fnorm );
+            temp2 = par * SQR( pnorm / fnorm );
+            prered = temp1 + 2 * temp2;
 
             /* scaled directional derivative */
-            dirder = -(SQR(temp1) + SQR(temp2));
+            dirder = -temp1 + temp2;
 
             /* ratio of actual to predicted reduction */
             ratio = prered != 0 ? actred / prered : 0;
@@ -410,7 +410,7 @@ void lmmin( int n, double *x, int m, const void *data,
                        " sq1 %.3e sq2 %.3e dd %.3e\n",
                        pnorm, delta, par,
                        actred, prered, prered != 0 ? ratio : 0.,
-                       SQR(temp1), SQR(temp2), dirder);
+                       temp1, temp2, dirder);
 
 /***  [inner]  Update the step bound.  ***/
 

commit c17a5f328eec05a7749d9b27e7fbc144e7f81751

Correct sign error introduced in https://jugit.fz-juelich.de/mlz/lmfit/-/commit/16341898ffcc4006ed79081819d01b997ad8f9d8#b0e2bcfc4ddf4d97538b18dc2e65be2184e43796, detected by Jannik Schürg.

lib/lmmin.c
@@ -452,7 +452,7 @@ void lmmin2(
                 goto terminate;
             }
             prered = temp1 + 2 * temp2;
-            dirder = -temp1 + temp2; /* scaled directional derivative */
+            dirder = -temp1 - temp2; /* scaled directional derivative */
 
             /* at first call, adjust the initial step bound. */
             if ( !outer && !inner && pnorm < delta )