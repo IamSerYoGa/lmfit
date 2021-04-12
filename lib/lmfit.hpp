1a678f67739b445cbf0e5f99f62290ebfbf55f45
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2021-04-12 15:56:27 +0200

    simplify return data type

--- lib/lmfit.hpp
+++ lib/lmfit.hpp
@@ -20,17 +20,17 @@ extern "C" {
 
 namespace lmfit{
 
-typedef struct lm_result_struct{
+struct result_t {
     std::vector<double> par;
     std::vector<double> parerr;
     std::vector<double> covar;
     lm_status_struct status;
-    lm_result_struct(const std::vector<double>& p0)
+    result_t(const std::vector<double>& p0)
         : par(p0), parerr(p0.size()),
           covar(p0.size()*p0.size()) {}
-} lm_result_struct;
+};
 
-lm_result_struct minimize(std::vector<double>& start_par,
+result_t minimize(std::vector<double>& start_par,
                         const void *const data, const int m_dat,
                         void (*const evaluate)(
                             const double *const par, const int m_dat,
@@ -38,7 +38,7 @@ lm_result_struct minimize(std::vector<double>& start_par,
                             int *const userbreak),
                         const lm_control_struct& control)
 {
-    lm_result_struct res(start_par);
+    result_t res(start_par);
 
     lmmin2(start_par.size(), res.par.data(), res.parerr.data(),
            res.covar.data(), m_dat, nullptr, data, evaluate, &control,
@@ -47,7 +47,7 @@ lm_result_struct minimize(std::vector<double>& start_par,
     return res;
 }
 
-lm_result_struct fit(std::vector<double>& start_par,
+result_t fit(std::vector<double>& start_par,
                      std::vector<double>& y, const void *const data,
                      void (*const evaluate)(
                             const double *const par, const int m_dat,
@@ -55,7 +55,7 @@ lm_result_struct fit(std::vector<double>& start_par,
                             double *const fvec, int *const userbreak),
                      const lm_control_struct& control)
 {
-    lm_result_struct res(start_par);
+    result_t res(start_par);
 
     lmmin2(start_par.size(), res.par.data(), res.parerr.data(),
            res.covar.data(), y.size(), y.data(), data, evaluate, &control,
@@ -64,13 +64,13 @@ lm_result_struct fit(std::vector<double>& start_par,
     return res;
 }
 
-lm_result_struct fit_curve(std::vector<double>& par,
+result_t fit_curve(std::vector<double>& par,
                     const std::vector<double>& t, const std::vector<double>& y,
                     double (*g)(const double t, const double* par),
                     const lm_control_struct& control)
 {
     assert (t.size() == y.size());
-    lm_result_struct res(par);
+    result_t res(par);
     lmcurve(par.size(), res.par.data(), t.size(), t.data(), y.data(), g,
             &control, &res.status);
     return res;
