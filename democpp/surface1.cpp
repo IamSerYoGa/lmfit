c1c1e9d662bdee9e4bb6846a6613a998cbc848c7
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2021-04-12 15:32:26 +0200

    democpp/surface1 add const, and other C++ style improvements

--- democpp/surface1.cpp
+++ democpp/surface1.cpp
@@ -7,52 +7,52 @@ double f(double tx, double tz, const double *p)
 }
 
 typedef struct {
-    double *tx, *tz;
-    double *y;
-    double (*f)(double tx, double tz, const double *p);
+    const double *tx;
+    const double *tz;
+    const double *y;
+    double (*const f)(double tx, double tz, const double *p);
 } data_struct;
 
 void evaluate_surface(const double *par, int m_dat, const void *data,
                       double *fvec, int *info)
 {
-    data_struct *D;
-    D = (data_struct*)data;
+    data_struct *D = (data_struct*)data;
 
-    int i;
-    for (i = 0; i < m_dat; i++)
+    for (int i = 0; i < m_dat; i++)
         fvec[i] = D->y[i] - D->f(D->tx[i], D->tz[i], par);
 }
 
 int main()
 {
-    std::vector<double> par{ -1, 0, 1 };
+    std::vector<double> par0{ -1, 0, 1 };
 
-    int m_dat = 4;
-    std::vector<double> tx{ -1, -1, 1, 1};
-    std::vector<double> tz{ -1, 1, -1, 1};
-    std::vector<double> y{ 0, 1, 1, 2};
+    const std::vector<double> tx{ -1, -1, 1, 1};
+    const std::vector<double> tz{ -1, 1, -1, 1};
+    const std::vector<double> y{ 0, 1, 1, 2};
+    const int m_dat = y.size();
+    assert((int) tx.size() == m_dat);
+    assert((int) tz.size() == m_dat);
 
-    data_struct data = {tx.data(), tz.data(), y.data(), f};
+    const data_struct data = {tx.data(), tz.data(), y.data(), f};
 
     lm_control_struct control = lm_control_double;
     control.verbosity = 9;
 
     std::cout << "Fitting:" << '\n';
-    auto result = lmfit::minimize(par, (const void*) &data, m_dat,
-                                &evaluate_surface, &control);
+    auto result = lmfit::minimize(par0, (const void*) &data, m_dat, &evaluate_surface, &control);
 
     std::cout << "\nResults:" << '\n';
     std::cout << "status after " << result.status.nfev
               << " function evaluations:\n" << lm_infmsg[result.status.outcome]
               << '\n';
     std::cout << "obtained parameters:" << '\n';
-    for (size_t j = 0; j < par.size(); ++j)
+    for (size_t j = 0; j < result.par.size(); ++j)
         std::cout << "par[" << j << "] = " << result.par[j] << '\n';
 
-    if (result.status.fnorm < 1e-14) {
-        std::cout << "SUCCESS (obtained norm = " << result.status.fnorm << ")\n";
-        return 0;
+    if (result.status.fnorm > 1e-14) {
+        std::cout << "FAILURE (obtained norm = " << result.status.fnorm << " is too large)\n";
+        return 1;
     }
-    std::cout << "FAILURE (obtained norm = " << result.status.fnorm << " is too large)\n";
-    return 1;
+    std::cout << "SUCCESS (obtained norm = " << result.status.fnorm << ")\n";
+    return 0;
 }
